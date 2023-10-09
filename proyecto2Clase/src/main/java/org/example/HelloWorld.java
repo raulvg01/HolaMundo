3841d42fc1f07bc983abd4dea69bf29568100d8f
Jorge Berlanga <jorgeberlangaheredia@gmail.com>
2023-10-09 17:01:47 +0200

    Característica: Muestra la fecha actual.

--- proyecto2Clase/src/main/java/org/example/HelloWorld.java
+++ proyecto2Clase/src/main/java/org/example/HelloWorld.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -6,5 +7,9 @@ public class HelloWorld {
         System.out.println("¿Cuál es tu nombre?");
         String name = scanner.nextLine();
         System.out.println("¡Hola " + name + "!");
+        System.out.println("¡Hola " + name + "!");
+        System.out.println("Hoy es: " + LocalDate.now());
     }
+
+
 }
